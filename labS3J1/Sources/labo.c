db03504c3d14304510a56af779611eeddb486998
Jessie Leclerc <2330032@bart.ca>
2024-02-15 17:21:42 -0500

    Avancement lab.S3J1

--- labS3J1/Sources/labo.c
+++ labS3J1/Sources/labo.c
@@ -5,24 +5,28 @@
 /*
 * Creer un noeud et l'ajouter apres le noeud currNode. Utiliser la fonction allocate pour allouer de la memoire.
 */
-void insert(Node* currNode, void* newData){
-	Node* n =  (Node*)allocate(sizeof(Node));
+void insert(Node* currNode, void* newData) {
+	Node* n = (Node*)allocate(sizeof(Node));
 	n->data = newData;
 	n->next = NULL;
 
 	if (currNode->next != NULL) {
-		n->next = currNode->next;
+		Node* temp = currNode->next;
+		currNode->next = n;
+		n->next = temp;
 	}
-	currNode->next = n;
+	else {
+		currNode->next = n;
+	}
 }
 
 /*
 * Creer un noeud et l'ajouter apres le noeud head. Si le noeud head est vide(data) lui donner la nouvelle valeur passer.
 */
 
-void insertHead(Node* head, void* newData){
+void insertHead(Node * head, void* newData){
 	
-	if (head->data != NULL) {
+	if (head != NULL) {
 		insert(head, newData);
 	}
 	else {
@@ -39,18 +43,25 @@ Node* removeByData(Node* head, void* rmData){
 	Node* temp = head;
 	Node* last = NULL;
 
-	while (temp != NULL && (int)temp->data != rmData) {
+	while (temp != NULL && temp->data != rmData) {
 		last = temp;
 		temp = temp->next;
 	}
 	if (temp == NULL) {
-		return "Error";
+		return NULL;
 	}
-	last->next = temp->next; 
+	if (last == NULL) {
+		head = temp->next;
+	}
+	else {
+		last->next = temp->next; 
+	}
+	return head;
 }
 
 /*
-* Supprimer le noeud ayant le nom passer par name.Par simplicite, pour supprimer faite un memset(node,0,sizeof(Node));
+* Supprimer le noeud ayant le nom passer par name. 
+* Par simplicite, pour supprimer faite un memset(node,0,sizeof(Node));
 * Retourner le noeud juste avant celui supprimer
 */
 
