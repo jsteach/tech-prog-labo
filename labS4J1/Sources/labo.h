025ce7ebc64e3662691910a93066a9ae89bed610
Jessie Leclerc <2330032@bart.ca>
2024-02-29 15:35:51 -0500

    Update lab.S4J1 & lab.S4J2

--- labS4J1/Sources/labo.h
+++ labS4J1/Sources/labo.h
@@ -4,8 +4,14 @@
 void* allocate(size_t size);
 
 /*
-* Declarer une structure de donner appeler Stack qui contient une propriete, max_size de type size_t,top de type size_t et un tableau de type void**.
+* Declarer une structure de donner appeler Stack qui contient une propriete 
+  max_size de type size_t,top de type size_t et un tableau de type void**.
 */
+typedef struct Stack {
+	size_t max_size;
+	size_t top;
+	void** data;
+}Stack;
 
 typedef struct person_t {
 	char name[256];
@@ -13,7 +19,8 @@ typedef struct person_t {
 } Person;
 
 /*
-* Creer la fonction d'initialization. Utiliser la fonction allocate au lieu de malloc.
+* Creer la fonction d'initialization. 
+  Utiliser la fonction allocate au lieu de malloc.
 */
 Stack* new_stack(size_t max_size);
 
@@ -23,21 +30,26 @@ Stack* new_stack(size_t max_size);
 void push(Stack* s, void* newData);
 
 /*
-* Enlever l'element de la stack et retourner l'information. Si jamais il n'y a pas d'element, retourner NULL.
+* Enlever l'element de la stack et retourner l'information. 
+  Si jamais il n'y a pas d'element, retourner NULL.
 */
 void* pop(Stack* s);
 
 /*
-* Retourner l'element du top sans l'enlever de la stack. Si jamais il n'y a pas d'element, retourner NULL.
+* Retourner l'element du top sans l'enlever de la stack. 
+  Si jamais il n'y a pas d'element, retourner NULL.
 */
 void* peek(Stack* s);
 
 /*
-* Inverser l'ordre de la stack i.e. le haut va devenir le bas.Vous ne devez pas utilise la variable temp qui est une autre Stack.
+* Inverser l'ordre de la stack i.e. le haut va devenir le bas.
+  Vous ne devez pas utilise la variable temp qui est une autre Stack.
 */
 void reverseStack(Stack* s);
 
 /*
-* Triee la stack.La personne sur le haut/top est la plus jeune. Vous ne pouvez pas modifier la propriete data directement. Vous devez utiliser push,pop et/ou peek.
+* Triee la stack.La personne sur le haut/top est la plus jeune. 
+  Vous ne pouvez pas modifier la propriete data directement. 
+  Vous devez utiliser push,pop et/ou peek.
 */
 void sortStack(Stack* s);
--- labS4J2/Sources/labo.h
+++ labS4J2/Sources/labo.h
@@ -1,13 +1,25 @@
 #include <stdint.h>
 #include <assert.h>
+#include <stdio.h>
 
 
 
 void* allocate(size_t size);
 
 /*
-* Declarer une structure de donnee appeler Queue qui est un noeud comme le Node de la liste doublement chainee. Declare aussi un noeud qui va avoir le nom Node.
+* Declarer une structure de donnee appeler Queue qui est un noeud comme le Node de la liste doublement chainee.
+  Declare aussi un noeud qui va avoir le nom Node.
 */
+
+typedef struct Node Node; 
+
+struct Node {
+	void* data;
+	Node* prev;
+	Node* next;
+};
+
+typedef Node Queue;
 
 typedef struct person_t {
 	char name[256];
@@ -20,19 +32,22 @@ typedef struct person_t {
 void push(Queue* q, Node* n);
 
 /*
-* Enlever l'element de la queue/file et retourner le noeud. Si jamais il n'y a pas de noeud, retourner NULL.
+* Enlever l'element de la queue/file et retourner le noeud. 
+  Si jamais il n'y a pas de noeud, retourner NULL.
 */
 Node* pop(Queue* q);
 
 
 
 /*
-* Retourner l'element de la fin de la queue/file sans l'enlever de la queue. Si jamais il n'y a pas d'element, retourner NULL.
+* Retourner l'element de la fin de la queue/file sans l'enlever de la queue. 
+  Si jamais il n'y a pas d'element, retourner NULL.
 */
 Node* peek(Queue* q);
 
 /*
-* Ajouter l'element sur la queue/file comme si elle serait une priority queue. Utiliser l'age afin de "trie" a chaque push.La personne au premier pop() est la plus jeune.
+* Ajouter l'element sur la queue/file comme si elle serait une priority queue. 
+  Utiliser l'age afin de "trie" a chaque push.La personne au premier pop() est la plus jeune.
 * On utilise pas une fonction de tri.
 */
 void pushAsPriorityQueue(Queue* q, Node* n);
